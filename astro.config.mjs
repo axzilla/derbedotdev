e776fd0900d240e398ed49be618db7e04f308100
axzilla <chanter-acidic-0r@icloud.com>
2023-03-03 11:45:32 +0100

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  site: "https://www.derbe.dev",
+  output: 'server',
+  adapter: vercel({
+    analytics: true
+  }),
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  })],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
