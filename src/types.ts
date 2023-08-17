ec89cb9c7a02e2a319134c04867f5f9d0945bbf8
axzilla <chanter-acidic-0r@icloud.com>
2023-08-17 21:04:41 +0200

    WIP - do soft reset

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,22 @@
-export interface ToolsItem {
-  icon: string;
-  title: string;
-  url: string;
-}
 
 export interface CvItem {
-  date: string;
-  tags: string[];
-  items: string[];
+    date: string;
+    tags: string[];
+    items: string[];
 }
 
 export interface FaqItem {
-  question: string;
-  answer: string;
-}
-
-export interface FooterLink {
-  description: string;
-  icon: string;
-  url: string;
+    question: string;
+    answer: string;
 }
 
 export interface NavItem {
-  title: string;
-  url: string;
+    title: string;
+    url: string;
 }
 
 export interface NotARobotCard {
-  title: string;
-  text: string;
-  image: ImageMetadata;
+    title: string;
+    text: string;
+    image: ImageMetadata;
 }
